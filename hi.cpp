32f4ffefa8bddf202d1ac3f761b668f820f8123c
Samuel A. Falvo II <kc5tja@arrl.net>
2014-03-29 21:52:04 -0700

    Use normal conventions, and introduce actual menu option.

--- hi.cpp
+++ hi.cpp
@@ -8,6 +8,12 @@
 #include <wx/wx.h>
 #endif
 
+/****** Command Identifiers ******/
+
+enum {
+	ID_HELLO = 1
+};
+
 /****** My Window ******/
 
 class HiWindow : public wxFrame {
@@ -17,16 +23,13 @@ public:
 
 HiWindow::HiWindow(const wxString& title, const wxPoint& pos, const wxSize& size)
 		: wxFrame(NULL, wxID_ANY, title, pos, size) {
-	wxMenu *m = new wxMenu;
-	m->Append(wxID_NEW);
-	m->Append(wxID_OPEN);
-	m->Append(wxID_SAVE);
-	m->Append(wxID_SAVEAS);
-	m->AppendSeparator();
-	m->Append(wxID_EXIT);
+	wxMenu *mFile = new wxMenu;
+	mFile->Append(ID_HELLO, "Hello &Mother...\tCtrl-M");
+	mFile->AppendSeparator();
+	mFile->Append(wxID_EXIT);
 
 	wxMenuBar *mb = new wxMenuBar;
-	mb->Append(m, "&Project");
+	mb->Append(mFile, "&File");
 	SetMenuBar(mb);
 
 	CreateStatusBar();
